c1918e582ede95239cc89c04bc7da24467acdebc
Evgen <zhenyakeptyuha@mail.ru>
2022-06-05 23:58:56 +0300

    Сделал 13ю задачу

--- Семинар 2 C#/Task_13/Program.cs
+++ Семинар 2 C#/Task_13/Program.cs
@@ -1,3 +1,3 @@
-﻿Console.WriteLine("Введите число ->");
+﻿Console.WriteLine("Введите число -> ");
 int n = int.Parse(Console.ReadLine());
 Console.WriteLine(n > 99 ? n.ToString()[2] : "Третьей цифры нет");
