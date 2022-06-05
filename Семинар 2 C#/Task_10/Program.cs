9c8145ac05efd758afe8bea5c32fa51e1b0d0b1d
Evgen <zhenyakeptyuha@mail.ru>
2022-06-05 23:19:00 +0300

    Сделал 10ю задачу

---
+++ Семинар 2 C#/Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число ->");
+int num = int.Parse(Console.ReadLine());
+int last = num % 10;
+int second = num % 100;
+int Number = second - last;
+int num2 = Number / 10;
+Console.WriteLine($"Вторая цифра - {num2}");
