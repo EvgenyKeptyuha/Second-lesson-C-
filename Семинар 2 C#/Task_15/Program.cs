7370eb345390df4cefeb670fd004d5c8b74ba8e5
Evgen <zhenyakeptyuha@mail.ru>
2022-06-06 00:05:53 +0300

    Сделал 15ю задачу

---
+++ Семинар 2 C#/Task_15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите цифру соттветствующую дню недели");
+int n = int.Parse(Console.ReadLine());
+if (n == 6) Console.WriteLine("Да");
+else if (n == 7) Console.WriteLine("Да");
+else Console.WriteLine("нет");
