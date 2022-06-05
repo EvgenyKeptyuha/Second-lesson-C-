a6d992d735d37b5a389777706af2af7145b9b499
Evgen <zhenyakeptyuha@mail.ru>
2022-06-05 21:31:01 +0300

    решили 11ю задачу

---
+++ Семинар 2 C#/Task_11/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine($"Number = {num}");
+int last = num % 10;
+int first = num / 100;
+int res = first * 10 + last;
+Console.WriteLine($"Случайное число = {num}");
+Console.WriteLine($"Результат = {res}");
