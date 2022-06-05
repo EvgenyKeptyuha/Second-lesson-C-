5e46a1bacd798df222ed394bb1889e2728742058
Evgen <zhenyakeptyuha@mail.ru>
2022-06-05 21:54:33 +0300

    решили 12ю задачу

---
+++ Семинар 2 C#/Task_12/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число ->");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ->");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, остаток = {num1 % num2}");
